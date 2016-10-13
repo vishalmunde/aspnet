4d9ba216fdfdfcb68948e19e14f92b0a83d95f1a
Vishal Munde <vishalgmunde@gmail.com>
2016-10-13 14:45:56 -0700

    javascripts

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿//site.js
+(function () {
+
+    //var ele = $("#username");
+    //ele.text("Vishal Munde");
+    
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this)
+    //alert(me.text());
+    //});
+    var $siderbarAndWrapper = $("#sidebar", "#wrapper");
+    $("#sidebarToggle").on("click", function () {
+        $siderbarAndWrapper.toggleClass("hide-sidebar");
+    })
+
+    var $main = $("#main").on("click", function () {
+        $(this).attr("background-color", "Red");
+    })
+})();
